31472be03390730b91c2904282dda9161265a2e6
Kushang Shah <kushangshah131@gmail.com>
2020-10-09 09:04:28 +0530

    Update navigation.php

--- COMMON_HTML/navigation.php
+++ COMMON_HTML/navigation.php
@@ -4,7 +4,7 @@
     <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right kel-hover" href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
       <i class="fa fa-bars"></i>
     </a>
-    <a href="faq" class="w3-bar-item w3-button w3-hide-small kel-hover w3-hover-theme-dark w3-round w3-right"><i class="fa fa-question-circle"></i> FAQs</a>
+    <a href="faqs" class="w3-bar-item w3-button w3-hide-small kel-hover w3-hover-theme-dark w3-round w3-right"><i class="fa fa-question-circle"></i> FAQs</a>
     
     <a href="aboutus" class="w3-bar-item w3-button w3-hide-small kel-hover w3-hover-theme-dark w3-round"><i class="fa fa-users"></i> About us</a>
     
@@ -16,7 +16,7 @@
     
 <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-animate-top">
     <a href="register" class="w3-bar-item w3-button w3-theme-l1 w3-hover-theme-dark kel-hover-2" onclick="toggleFunction()"><i class="fa fa-user-plus"></i> Register</a>
-    <a href="faq" class="w3-bar-item w3-button w3-theme-l1 w3-hover-theme-dark kel-hover-2" onclick="toggleFunction()"><i class="fa fa-question-circle"></i> FAQs</a>
+    <a href="faqs" class="w3-bar-item w3-button w3-theme-l1 w3-hover-theme-dark kel-hover-2" onclick="toggleFunction()"><i class="fa fa-question-circle"></i> FAQs</a>
     <a href="aboutus" class="w3-bar-item w3-button w3-theme-l1 w3-hover-theme-dark kel-hover-2" onclick="toggleFunction()"><i class="fa fa-users"></i> About us</a>
     <a href="contact" class="w3-bar-item w3-button w3-theme-l1 w3-hover-theme-dark kel-hover-2" onclick="toggleFunction()"><i class="fa fa-envelope"></i> Contact</a>
     <a href="login" class="w3-bar-item w3-button w3-theme-l1 w3-hover-theme-dark kel-hover-2" onclick="toggleFunction()"><i class="fa fa-sign-in"></i> Log in</a>
