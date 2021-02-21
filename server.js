49b583a5f79d6ce7c34bca85ff1c32a3ad3caf0d
Kushang Shah <kushangshah131@gmail.com>
2021-02-21 12:05:17 +0530

    add

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log("Yo");
+const express = require("express");
+const bodyparser = require("body-parser");
+const app = express();
+
+
+app.listen(3000, () => {
+    console.log("Listeneing at 3000");
+})
+
+app.use(bodyparser.urlencoded({extended:true}))
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/Views/index.html");
+})
+
+app.get("/:id", function(req, res){
+    let obj = req.params;
+    let id = obj['id'];
+    res.send(id);
+});
+
+app.post("/quotes", function(req, res){
+    res.send(req.body);
+})
