fe52fd8be40213679f489bc404be4ed4a083e811
Kushang Shah <kushangshah131@gmail.com>
2021-02-22 08:49:23 +0530

    add all

--- server.js
+++ server.js
@@ -2,41 +2,46 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://kingman:test123@cluster0.zmum7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient();
 
 const app = express();
-
+app.set("View engine", "ejs");
 app.listen(3000, () => {
     console.log("Listeneing at 3000");
 });
 
-client.connect((err, client) => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    if(err){
-        console.log("Error is: "+err);
-        return false;
-    }
-    else{
-        console.log("Connected");
-    }
+MongoClient.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+}).then(client => {
+        const collection = client.db("test").collection("devices");
+        // perform actions on the collection object
     
-    const db = client.db('star-wars');
-    client.close();
-});
+        console.log("Connected");
+        
+        const db = client.db('star-wars');
+        const quotesCollection = db.collection('quotes');
+        app.use(bodyparser.urlencoded({extended:true}));
 
-app.use(bodyparser.urlencoded({extended:true}));
+        app.get("/", function(req, res){
+            
+            db.collection('quotes').find().toArray().then(results => {
+                res.render('index.ejs', {quotes: results});
+            })
+            
+        });
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/Views/index.html");
-});
+        app.get("/:id", function(req, res){
+            let obj = req.params;
+            let id = obj['id'];
+            res.send(id);
+        });
 
-app.get("/:id", function(req, res){
-    let obj = req.params;
-    let id = obj['id'];
-    res.send(id);
-});
+        app.post("/quotes", function(req, res){
+            quotesCollection.insertOne(req.body);
+            res.redirect("/");
+        });
 
-app.post("/quotes", function(req, res){
-    res.send(req.body);
-});
+})
+.catch(error => console.log(error))
+
