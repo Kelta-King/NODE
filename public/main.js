d60d9a4727f82bb316cba47985205e3e402a4bde
Kushang Shah <kushangshah131@gmail.com>
2021-02-22 11:06:24 +0530

    add Quotes

--- public/main.js
+++ public/main.js
@@ -1,14 +1,55 @@
-document.getElementById('updt').addEventListener('click', (evt) => {
 
-    fetch('/quotes', {
-        method:'PUT',
-        headers: {
-            'Content-type':'application/json',
-        },
+document.querySelector("#addQuote").addEventListener('click', () => {
+    
+    let loader = document.querySelector("#loader-add");
+    let error = document.getElementById('error-add');
+
+    loader.style.display = "block";
+
+    let name = document.querySelector("#name").value;
+    let quote = document.querySelector("#quote").value;
+
+    if(name == ""){
+        error.innerHTML = "Write name";
+        return false;
+    }
+    if(quote == ""){
+        error.innerHTML = "Write Quote";
+        return false;
+    }
+    
+    fetch('/addQuote', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-            name:"Dart Vader",
-            quote:"World is wnding",
+          name: name,
+          quote: quote
         })
+    })
+    .then(res => {
+        console.log(res)
+        loader.style.display = "none";
+        location.reload();
     })
 
 });
+
+let deleteQuote = (id) => {
+
+    if(!confirm("Do you really want to remove this quote?")){
+        return false;
+    }
+
+    fetch('/deleteQuote', {
+        method: 'delete',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          id:id,
+        })
+    })
+    .then(res => {
+        console.log(res)
+        //location.reload();
+    })
+
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const MongoClient = require('mongodb').MongoClient;
+const { response } = require("express");
 const uri = "mongodb+srv://kingman:test123@cluster0.zmum7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient();
 
@@ -14,60 +15,88 @@ MongoClient.connect(uri, {
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 }).then(client => {
-        const collection = client.db("test").collection("devices");
-        // perform actions on the collection object
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
     
-        console.log("Connected");
+    console.log("Connected");
         
-        const db = client.db('star-wars');
-        const quotesCollection = db.collection('quotes');
-        app.use(bodyparser.urlencoded({extended:true}));
+    const db = client.db('star-wars');
+    const quotesCollection = db.collection('quotes');
+    app.use(bodyparser.urlencoded({extended:true}));
         
-        app.use(express.static('public'));
-        app.use(bodyparser.json());
+    app.use(express.static('public'));
+    app.use(bodyparser.json());
 
-        app.get("/", function(req, res){
-            
-            db.collection('quotes').find().toArray().then(results => {
-                res.render('index.ejs', {quotes: results});
-            })
+    app.get("/", function(req, res){
+        
+        db.collection('quotes').find().toArray().then(results => {
+            res.render('index.ejs', {quotes: results});
+        })
 
-        });
+    });
+    
+    app.post("/addQuote", function(req, res){
 
-        app.get("/:id", function(req, res){
-            let obj = req.params;
-            let id = obj['id'];
-            res.send(id);
-        });
+        quotesCollection.insertOne(req.body).then(result => {
+            res.json('Success');
+        })
+        .catch(err => {
+            console.log(err);
+        })
+        
 
-        app.post("/quotes", function(req, res){
-            quotesCollection.insertOne(req.body);
-            res.redirect("/");
-        });
+    });
 
-        app.put("/quotes", function(req, res){
+    app.delete("/deleteQuote", function(req, res){
+        console.log(req.body.id);
+        quotesCollection.deleteOne(
+            {
+                _id:req.body.id
+            }
+        )
+        .then(result => {
 
-            quotesCollection.findOneAndUpdate(
-                {name: "ghjj"},
-                {
-                    $set: {
-                        name: req.body.name,
-                        quote: req.body.quote
-                    }
-                },
-                {
-                    upsert:true
+            if(result.deletedCount === 0){
+                return res.json("No quotes of this ID");
+            }
+            console.log(err);
+
+        })
+        .catch(err => {
+            console.log(err);
+        })
+        
+
+    });
+    /*
+    app.post("/quotes", function(req, res){
+        quotesCollection.insertOne(req.body);
+        res.redirect("/");
+    });
+
+    app.put("/quotes", function(req, res){
+
+        quotesCollection.findOneAndUpdate(
+            {name: "ghjj"},
+            {
+                $set: {
+                    name: req.body.name,
+                    quote: req.body.quote
                 }
+            },
+            {
+                upsert:true
+            }
 
-            ).then(result => {
-                console.log(result);
-            })
-            .catch(err => {
-                console.log(err);
-            })
+        ).then(result => {
+            console.log(result);
+        })
+        .catch(err => {
+            console.log(err);
+        })
 
-        });
-
+    });
+    */
 })
 .catch(error => console.log(error))
 
